728cc4a62862cb47f1c46209ba2dafd85d2b0a58
Shengjia <sjzhao@stanford.edu>
2021-06-26 22:35:11 -0700

    Add CRPS

--- torchuq/metric/distribution.py
+++ torchuq/metric/distribution.py
@@ -42,3 +42,27 @@ def plot_icdf(predictions, labels):
     plt.ylabel('cdf')
     plt.xlabel('value')
     
+
+def compute_crps(predictions, labels, resolution=500):  
+    """
+    Compute the CRPS score 
+    
+    Input:
+        predictions: a batch of distribution predictions
+        labels: array [batch_size] of labels
+        resolution: the discretization bins, higher resolution increases estimation accuracy but also requires more memory/compute
+        
+    Output:
+        crps: array [batch_size], the CRPS score 
+    """
+    intervals = torch.linspace(0, 1, resolution+2)[1:-1].view(-1, 1)
+    weights = (intervals ** 2)[1:] - (intervals ** 2)[:-1]   # This is a trick to compute the Lesbegue integral of CDF^2 (when we only have access to inverse CDF)
+    weights = torch.cat([weights[:1], (weights[1:] + weights[:-1])/2, weights[-1:]], dim=0)   # Handle the boundary 
+
+    icdf = predictions.icdf(intervals)  
+    partition = (labels.view(1, -1) > icdf).type(torch.float32)  # Partition the intervals based on whether it's greater or less than the labels. This biases the gradient, so use with caution
+    part_under = (labels - icdf) * weights * partition   # Compute the Lesbegue integral for the \int_{x, x < y} (F[x] - I(y < x))^2 dx
+    part_over = (icdf - labels) * weights.flip(dims=[0]) * (1 - partition)  # Compute the Lesbegue integral for the \int_{x, x > y} (F[x] - I(y < x))^2 dx
+    crps = (part_under + part_over).abs().sum(dim=0)   # The abs is not necessary, but placed here just in case numerical errors cause CRPS to be below zero. 
+    return crps
+  
