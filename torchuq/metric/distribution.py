65baa224e70c0da66f624fe2c8b4275c1bc3e45e
Shengjia <sjzhao@stanford.edu>
2021-06-27 09:09:01 -0700

    Add plots for reliability diagram and density

--- torchuq/metric/distribution.py
+++ torchuq/metric/distribution.py
@@ -66,3 +66,71 @@ def compute_crps(predictions, labels, resolution=500):
     crps = (part_under + part_over).abs().sum(dim=0)   # The abs is not necessary, but placed here just in case numerical errors cause CRPS to be below zero. 
     return crps
   
+
+    
+def plot_reliability_diagram(predictions, labels, ax=None, plot_confidence=True):
+    """
+    Plot the reliability diagram https://arxiv.org/abs/1807.00263 
+    
+    Input:
+        predictions: required Distribution instance, a batch of distribution predictions
+        labels: required array [batch_size], the labels
+        ax: optional matplotlib.axes.Axes, the axes to plot the figure on, if None automatically creates a figure with recommended size 
+        plot_confidence: optional boolean, if set to true also plot the confidence interval. This is the range we expect the reliability diagram to fluctuate even when the predictions are perfect. 
+    """
+    with torch.no_grad():
+        cdfs = predictions.cdf(labels).flatten() 
+        cdfs, _ = torch.sort(cdfs) 
+        
+        if ax is None:
+            plt.figure(figsize=(5, 5))
+            ax = plt.gca() 
+
+        # Use bootstrap to compute if the predictions are correct, what is the range of the reliability diagram 
+        # I think it is possible to compute this in closed form, but bootstrap should work well enough
+        if plot_confidence:
+            samples = torch.rand(100, len(cdfs))
+            samples, _ = torch.sort(samples, dim=1)
+            min_curve, _ = samples.min(dim=0)
+            max_curve, _ = samples.max(dim=0)
+            ax.fill_between(np.linspace(0, 1, len(cdfs)), min_curve.cpu(), max_curve.cpu(), alpha=0.1, color='C1')
+        
+        ax.plot(cdfs.cpu().numpy(), np.linspace(0, 1, len(cdfs)), c='C0')
+        ax.plot([0,1], [0,1], c='C1', linestyle=':')
+        ax.set_xlim([0, 1])
+        ax.set_ylim([0, 1])
+        ax.tick_params(axis='both', which='major', labelsize=14)
+        ax.set_xlabel('quantiles', fontsize=14)
+        ax.set_ylabel('proportion samples below quantile', fontsize=14)
+        
+        
+        
+def plot_density(predictions, labels, max_count=100, ax=None):
+    """ 
+    Plot the PDF of the predictions and the labels. For aesthetics the PDFs are reflected along y axis to make a symmetric violin shaped plot
+    
+    Input:
+        predictions: required Distribution instance, a batch of distribution predictions
+        labels: required array [batch_size], the labels
+        ax: optional matplotlib.axes.Axes, the axes to plot the figure on, if None automatically creates a figure with recommended size 
+        max_count: optional int, the maximum number of PDFs to plot
+    """
+    resolution = 100
+    values = predictions.icdf(torch.linspace(0, 1, resolution+2)[1:-1].view(-1, 1)).cpu()
+    centers = (values[1:] + values[:-1]) * 0.5
+    density = 1. / resolution / (values[1:] - values[:-1])  # Compute the empirical density
+    vpstats = [{'coords': centers[:, i].numpy(), 'vals': density[:, i].numpy(),
+                'mean': values[:, i].mean(), 'median': values[resolution // 2, i], 
+                'min': values[0, i], 'max': values[-1, i]} for i in range(len(density)) if i < max_count]
+    if ax is None:
+        optimal_width = len(vpstats) / 5
+        if optimal_width < 4:
+            optimal_width = 4
+        plt.figure(figsize=(optimal_width, 4))
+        ax = plt.gca() 
+        
+    ax.violin(vpstats=vpstats, positions=range(len(vpstats)), showextrema=False, widths=0.7)
+    ax.scatter(range(len(vpstats)), labels[:len(vpstats)].cpu().numpy(), marker='x')
+    ax.set_ylabel('label value', fontsize=14)
+    ax.set_xlabel('sample index', fontsize=14)
+    ax.tick_params(axis='both', which='major', labelsize=14)
