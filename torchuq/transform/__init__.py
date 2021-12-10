3d7a9f735afd4ea30a304787ab90d80e064854a5
Shengjia <sjzhao@stanford.edu>
2021-12-09 23:51:53 -0800

    Automatically import sub-modules when loading

--- torchuq/transform/__init__.py
+++ torchuq/transform/__init__.py
@@ -1,6 +1,12 @@
 
 
-# from .basic import ConcatDistribution
+from . import basic
+from . import calibrate
+from . import conformal
+from . import decision
+from . import direct
+from . import utils 
+
 # from .conformal import ConformalCalibrator
 # from .decision import CalibratorDecision
 # from .calibrate import TemperatureScaling
